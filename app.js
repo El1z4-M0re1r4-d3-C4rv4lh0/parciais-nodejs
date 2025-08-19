a0d34b08c2424707d8fdfe33a0f72298e3a5d425
Eliza <elizamoreira47@gmail.com>
2025-08-18 23:46:33 -0300

    Atividade 2

--- app.js
+++ app.js
@@ -7,21 +7,14 @@ app.set('view engine', 'ejs');
 app.listen(3006);
 
 app.get('/', (req, res) => {
-
-     const blogs = [
-        {titulo: 'Chamander', conteudo: 'A ponta do seu rabo está constantemente em chamas e se for apagada pode resultar em sua morte'},
-        {titulo: 'Squirtle', conteudo: 'Seu casco reduz a resistência contra a água fazendo com que ele nade mais rápido'},
-        {titulo: 'Bulbasaur', conteudo: 'Ao evoluir o bulbo começa a desabrochar até se tornar uma grande flor na evolução final'},
-    ];
-
-    res.render('home', {titulo: 'Home - Página Inicial', blogs});
+    res.render('home', {titulo: 'Home - Página Inicial'});
 });
 
 app.get('/servicos', (req, res) => {
     res.render('servicos', {titulo: 'Nossos Serviços'});
 });
 
-app.get('/sobrenos', (req, res) => {
+app.get('/sobre', (req, res) => {
     res.render('sobre', {titulo: 'Sobre Nós',});
 });
 
@@ -29,3 +22,6 @@ app.get('/contatos', (req, res) => {
     res.render('contatos', {titulo: 'Contatos - Fale Conosco',});
 });
 
+app.use((req, res) => {
+    res.status(404).render('404', {titulo: 'ERRO 404'});
+});
